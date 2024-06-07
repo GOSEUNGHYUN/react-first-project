978493e533f9443306424ac8caf545785bb70247
GOSEUNGHYUN <156834770+GOSEUNGHYUN@users.noreply.github.com>
2024-06-07 12:13:06 +0900

    6.4 Cleanup function

--- src/App.js
+++ src/App.js
@@ -1,39 +1,27 @@
-import Button from "./Button";
-import styles from "./App.module.css";
 import { useState, useEffect } from "react";
-function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onChange = (event) => setKeyword(event.target.value);
-  const onClick = () => setValue((prev) => prev + 1);
 
-  useEffect(() => {
-    console.log("나는 한번만 실행돼요.");
+function Hello() {
+  useEffect(function () {
+    console.log("hi :)");
+    return function () {
+      console.log("bye :(");
+    };
   }, []);
   useEffect(() => {
-    console.log("나는 '키워드'가 변화할 때만 실행돼요.");
-    /*if (keyword !== "" && keyword.length > 5) {
-      console.log("SEARCH FOR", keyword);
-    }*/
-  }, [keyword]); //[keyword]는 keyword가 변화할 때 코드를 실행할 거라고 react.js에게 알려주는 것이다.
+    //익명의 function을 만든다. 이 방법이 더 많이쓰이는방법이다.
+    console.log("hi :)");
+    return () => console.log("bye :("); //creanup function : destroy가 될 때 function을 실행 할 수 있다.
+  }, []);
+  return <h1>Hello</h1>; //hidden상태가 됬을 때 component를 숨기는 것이아니라 파괴된다.(콘솔창을 확인해보면 알 수 있다.) = destroy
+}
 
-  useEffect(() => {
-    console.log("나는 '카운터'가 변화할 때만 실행돼요.");
-  }, [counter]);
-
-  useEffect(() => {
-    console.log("나는 키워드,카운터 둘다 지켜보고 있고 변화할 때 실행돼요.");
-  }, [keyword, counter]);
+function App() {
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
   return (
     <div>
-      <input
-        value={keyword} //keyword를 가져와서 input의 value로 사용하면 우리가 원할 때 이 input을 조작할 수 있다.
-        onChange={onChange}
-        type="text"
-        placeholder="Search here"
-      />
-      <h1 className={styles.title}>{counter}</h1>
-      <button onClick={onClick}>Click me</button>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
     </div>
   );
 }

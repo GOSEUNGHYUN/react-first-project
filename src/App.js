c98304803754fbb3aa79cccaf5237cf6cd79d636
GOSEUNGHYUN <156834770+GOSEUNGHYUN@users.noreply.github.com>
2024-06-07 19:07:11 +0900

    #7.0 ToDoList만들기, State배열에 요소 추가하는 방법

--- src/App.js
+++ src/App.js
@@ -1,27 +1,30 @@
-import { useState, useEffect } from "react";
-
-function Hello() {
-  useEffect(function () {
-    console.log("hi :)");
-    return function () {
-      console.log("bye :(");
-    };
-  }, []);
-  useEffect(() => {
-    //익명의 function을 만든다. 이 방법이 더 많이쓰이는방법이다.
-    console.log("hi :)");
-    return () => console.log("bye :("); //creanup function : destroy가 될 때 function을 실행 할 수 있다.
-  }, []);
-  return <h1>Hello</h1>; //hidden상태가 됬을 때 component를 숨기는 것이아니라 파괴된다.(콘솔창을 확인해보면 알 수 있다.) = destroy
-}
+import { useState } from "react";
 
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing((prev) => !prev);
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") {
+      return;
+    }
+    setToDo("");
+    setToDos((currentArray) => [toDo, ...currentArray]); //배열에 요소를 추가하는 방법
+  };
+  console.log(toDos);
   return (
     <div>
-      {showing ? <Hello /> : null}
-      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+      <h1>My To Dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          type="text"
+          placeholder="Write your to do"
+        />
+        <button>Add To Do</button>
+      </form>
     </div>
   );
 }

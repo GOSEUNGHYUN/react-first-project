c40df11cdc22ecccf4c26c1fca604f5692fc7f98
GOSEUNGHYUN <156834770+GOSEUNGHYUN@users.noreply.github.com>
2024-06-07 09:58:29 +0900

    6.1 useEffect
    문제점 :  특정코드의 실행을 제한하고 싶다
    component가 맨 처음 render될 때만 말이야
    useEffect : 코드가 한번만 실행되고 다시는 실행하지 않는다.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import Button from "./Button";
 import styles from "./App.module.css";
+import { useState, useEffect } from "react";
 function App() {
+  const [counter, setValue] = useState(0);
+  const onClick = () => setValue((prev) => prev + 1);
+  console.log("i run all the time");
+
+  /*
+  const iRunOnlyOnce = () => {
+    console.log("i run only once.");
+  };
+  useEffect(iRunOnlyOnce, []);
+  */
+  useEffect(() => {
+    console.log("CALL THE API...");
+  }, []);
   return (
     <div>
-      <h1 className={styles.title}>Welcome badck!</h1>
-      <Button text={"Continue"} />
+      <h1 className={styles.title}>{counter}</h1>
+      <button onClick={onClick}>Click me</button>
     </div>
   );
 }

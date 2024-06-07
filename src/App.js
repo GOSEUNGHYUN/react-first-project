8d88277947a0d9b2609f1bf264ef19c5f28b80d6
GOSEUNGHYUN <156834770+GOSEUNGHYUN@users.noreply.github.com>
2024-06-07 19:48:45 +0900

    #7.1 ToDoList 만들기 완성

--- src/App.js
+++ src/App.js
@@ -10,9 +10,11 @@ function App() {
       return;
     }
     setToDo("");
-    setToDos((currentArray) => [toDo, ...currentArray]); //배열에 요소를 추가하는 방법
+    setToDos((currentArray) => [toDo, ...currentArray]);
+    //toDo를 받아와서 새로운 toDO의 array로 return하고 있는것이다.
   };
   console.log(toDos);
+  console.log(toDos.map((item, index) => <li key={index}>{item}</li>));
   return (
     <div>
       <h1>My To Dos ({toDos.length})</h1>
@@ -25,6 +27,32 @@ function App() {
         />
         <button>Add To Do</button>
       </form>
+      <hr />
+      {/*x
+      <ul>
+        {toDos.map((item) => (
+          <li>{item}</li>
+        ))}
+      </ul>
+      */}
+
+      <ul>
+        {toDos.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
+
+      {/*
+      toDos.map()
+      map은 이 안에 함수를 넣을수 있도록 해주는데
+      이 함수는 array의 모든 item에 대해서 실행하게된다.
+      여기에서 무엇을 return하던지간에 return한 값이 새로운 array에 들어가게 된다.
+      item이 6개가 있다면 내가 적은 함수가 6번 실행된다.
+
+      map은 함수의 첫번째 argument로 현재의 item을 가져 올 수 있다.
+      [].map((item) => item.toUpperCase()); 대문자로 바뀐 새로운 array
+      item의 이름은 직접 정할 수 있다. tomato, apple, ...
+      */}
     </div>
   );
 }

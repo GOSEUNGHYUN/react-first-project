6ae4fcb78919c07f23891f524896a46fa75274c9
GOSEUNGHYUN <156834770+GOSEUNGHYUN@users.noreply.github.com>
2024-06-07 22:35:23 +0900

    #7.2 Coin Tracker +챌린지

--- src/App.js
+++ src/App.js
@@ -1,58 +1,32 @@
-import { useState } from "react";
-
+import { useEffect, useState } from "react";
+/*챌린지
+input을 만들어서 내가 20달러를 가지고 있다고 쓸 수 있게하고
+20달러로 0.00의 비트코인을 가질 수 있다고 말해주는 등을 만들면된다. */
 function App() {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
-  const onChange = (event) => setToDo(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if (toDo === "") {
-      return;
-    }
-    setToDo("");
-    setToDos((currentArray) => [toDo, ...currentArray]);
-    //toDo를 받아와서 새로운 toDO의 array로 return하고 있는것이다.
-  };
-  console.log(toDos);
-  console.log(toDos.map((item, index) => <li key={index}>{item}</li>));
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
   return (
     <div>
-      <h1>My To Dos ({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          onChange={onChange}
-          value={toDo}
-          type="text"
-          placeholder="Write your to do"
-        />
-        <button>Add To Do</button>
-      </form>
-      <hr />
-      {/*x
-      <ul>
-        {toDos.map((item) => (
-          <li>{item}</li>
-        ))}
-      </ul>
-      */}
-
-      <ul>
-        {toDos.map((item, index) => (
-          <li key={index}>{item}</li>
-        ))}
-      </ul>
-
-      {/*
-      toDos.map()
-      map은 이 안에 함수를 넣을수 있도록 해주는데
-      이 함수는 array의 모든 item에 대해서 실행하게된다.
-      여기에서 무엇을 return하던지간에 return한 값이 새로운 array에 들어가게 된다.
-      item이 6개가 있다면 내가 적은 함수가 6번 실행된다.
-
-      map은 함수의 첫번째 argument로 현재의 item을 가져 올 수 있다.
-      [].map((item) => item.toUpperCase()); 대문자로 바뀐 새로운 array
-      item의 이름은 직접 정할 수 있다. tomato, apple, ...
-      */}
+      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
+      {loading ? (
+        <strong>Loading...</strong>
+      ) : (
+        <select>
+          {coins.map((coin) => (
+            <option>
+              {coin.name} ({coin.symbol}): {coin.quotes.USD.price} USD
+            </option>
+          ))}
+        </select>
+      )}
     </div>
   );
 }

a361551c1e20b23cee06915340d124a0d010b0a0
GOSEUNGHYUN <156834770+GOSEUNGHYUN@users.noreply.github.com>
2024-06-11 20:10:13 +0900

    배포 성공

--- src/App.js
+++ src/App.js
@@ -7,16 +7,14 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/movie/:id" element={<Detail />} />{" "}
-        {/* /movie/:id id가 Detail.js에서 useParams으로 내가 받게될 변수 이름이다. */}
-        <Route path="/" element={<Home />} />
+        <Route
+          path={`${process.env.PUBLIC_URL}/movie/:id`}
+          element={<Detail />}
+        />
+        {""}
+        <Route path={`${process.env.PUBLIC_URL}/`} element={<Home />} />
       </Routes>
     </Router>
   );
 }
 export default App;
-
-/* React Router는 다이나믹(동적) url을 지원해준다.
-다이나믹하다는 것은 url에 변수를 넣을 수 있다는 의미이다.
-http://localhost:3000/movie에서 http://localhost:3000/movie/:id(변수)를 입력하면 상세페이지를 보여준다.
- */
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -2,16 +2,10 @@ import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 
 function Movie({ id, coverImg, title, summary, genres }) {
-  //{ id, coverImg, title, summary, genres } = props(object) 하나의 오브젝트다.
-  //function Movie(props) {
-  //컴포넌트를 렌더링 한다는 것은 실질적으로는 함수를 불러오는 것이다.
-  //<Movie /> == Movie({object}) Movie함수를 호출해서 그 안에 이런식으로 object들을 전부 넘겨주고 있는것이다.
-  //props는 object일 뿐이고, 우리는 그걸 열어서 item을 꺼내 쓰는 것이다.
-
   return (
     <div>
       <h2>
-        <Link to={`/movie/${id}`}>{title}</Link>
+        <Link to={`${process.env.PUBLIC_URL}/movie/${id}`}>{title}</Link>
       </h2>
       <img src={coverImg} alt={title} />
       <p>{summary}</p>

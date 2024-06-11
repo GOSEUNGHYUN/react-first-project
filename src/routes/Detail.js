663389c85f3569bd0d34a4cc6abdafa6fc180fd0
GOSEUNGHYUN <156834770+GOSEUNGHYUN@users.noreply.github.com>
2024-06-11 19:58:44 +0900

    배포

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,29 +1,35 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 function Detail() {
-  const { id } = useParams(); //useParams이라는 함수를 사용하는거야
-  //이 함수를 사용하면 React Router는 바로 이 변수(app.js에 있는 id)의 값을 넘겨준다.
+  const [loading, setLoading] = useState(true);
+  const [page, setPage] = useState([]);
+
+  const { id } = useParams();
   const getMovie = async () => {
     const json = await (
       await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
     ).json();
-    console.log(json);
+    setPage(json);
+    setLoading(false);
   };
 
   useEffect(() => {
     getMovie();
   }, []);
-  return <h1>Detail</h1>;
+
+  return (
+    <div>
+      {loading ? <h1>loading...</h1> : <h1>{page.data.movie.title_long}</h1>}
+    </div>
+  );
 }
 
 export default Detail;
 
-/*
-<코드 챌린지>
-Detail스크린에 로딩만들기
-movie(json)가 state에있지않아. state에 넣어줘
-return 에 Detail에다가 영화의 상세정보를 보여줘
-이제 React.js는 다배웟어 몇 가지만 배우면 그다음에는 다 똑같아.
-그 다음에는 html이랑 그걸 예쁘게 만드는게 다야
-*/
+//npm i gh-pages 설치
+//package.json에 build라는 script가 있다.
+//이 script를 실행하면 우리 웹사이트의 production ready code를 생성하게된다.
+//production ready란 코드가 압축되고 모든게 최적화 된다는 의미이다.
+//build를 실행시킨다.(npm run build)
+//#7.7 1:18
